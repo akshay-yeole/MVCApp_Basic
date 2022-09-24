dabee0ac913d0a41f7c14a9ff71c071dde4403b8
Akshay Yeole <akshayyeole4366@gmail.com>
2022-09-24 18:47:31 +0530

    Add project files.

---
+++ MVCApp_Basic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MVCApp_Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
