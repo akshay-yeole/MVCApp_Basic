d8c2f7845b170d31c125827ded1015b3fb95ea28
Akshay Yeole <akshayyeole4366@gmail.com>
2022-09-25 10:41:48 +0530

    Controller with basic functionality

--- MVCApp_Basic/Controllers/HomeController.cs
+++ MVCApp_Basic/Controllers/HomeController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCApp_Basic.DataLayer;
+using MVCApp_Basic.Models;
+using System.Collections.Generic;
 
 namespace MVCApp_Basic.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        private readonly BookRepository  repository= null;
+
+        public HomeController()
         {
-            return "Welcome to home controller";
+            repository = new BookRepository();
+        }
+        public List<BookModel> Index()
+        {
+            return repository.GetAllBooks(); 
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return repository.GetBookById(id);
+        }
+
+        public BookModel SearchBook(string title, string author)
+        {
+            return repository.SearchBook(title, author);
         }
     }
 }
---
+++ MVCApp_Basic/DataLayer/BookRepository.cs
@@ -0,0 +1,33 @@
+﻿using MVCApp_Basic.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MVCApp_Basic.DataLayer
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public BookModel SearchBook(string title, string author)
+        {
+            return DataSource().Where(x => x.Title == title && x.Author == author).FirstOrDefault();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>() {
+                new BookModel{ Id=1, Title="ABC", Author="ABC"},
+                new BookModel{ Id=2, Title="XYZ", Author="XYZ"},
+                new BookModel{ Id=3, Title="JKL", Author="XYZ"}
+            };
+        }
+    }
+}
