d173193a390bb5e420a8ed3bdabdf8c13681d456
Akshay Yeole <akshayyeole4366@gmail.com>
2022-09-25 09:44:53 +0530

    Middlewares

--- MVCApp_Basic/StartUp.cs
+++ MVCApp_Basic/StartUp.cs
@@ -13,10 +13,18 @@ namespace MVCApp_Basic
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.Use(async (context,next) => {
+                await context.Response.WriteAsync("Exceuting Middleware1");
+                await next();
+                await context.Response.WriteAsync("\nExceuted  Middleware1");
+            });
+
             app.UseRouting();
-            app.UseEndpoints(endpoint => {
-                endpoint.MapGet("/", async context => {
-                    await context.Response.WriteAsync("Hello user");
+            app.UseEndpoints(endpoint =>
+            {
+                endpoint.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("\nHello user From MVC App");
                 });
             });
         }
