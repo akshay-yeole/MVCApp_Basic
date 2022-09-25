6710c076025b318ada10652e03f045d5c675195f
Akshay Yeole <akshayyeole4366@gmail.com>
2022-09-25 10:23:51 +0530

    Book Model Added

---
+++ MVCApp_Basic/Models/BookModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCApp_Basic.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
